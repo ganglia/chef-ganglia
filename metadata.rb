03445d24dcc42d9c3db5f75207e2376a265e44d1
Michael F. Weinberg <michael@luckymike.com>
2011-11-10 16:15:37 -0800

    Metadata update.

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,12 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.7"
 
-supports "debian"
-supports "ubuntu"
-supports "redhat"
-supports "centos"
-supports "fedora"
+%w{ debian ubuntu redhat centos fedora }.each do |os|
+  supports os
+end
+
+recommends "graphite"
+suggests "iptables"
+
