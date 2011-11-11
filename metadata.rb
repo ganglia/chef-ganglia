efe743d36d1e9b3abfa429ef93ce42b633228836
Michael F. Weinberg <michael@luckymike.com>
2011-11-11 12:18:06 -0800

    Graphite integration fixes. Bumped version to 0.0.9.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.8"
+version          "0.0.9"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
--- recipes/graphite.rb
+++ recipes/graphite.rb
@@ -1,7 +1,12 @@
-include_recipe "graphite"
+graphite_host = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}
+if graphite_host.empty?
+  graphite_host = "localhost"
+end
 
 template "/usr/local/sbin/ganglia_graphite.rb" do
   source "ganglia_graphite.rb.erb"
+  mode "744"
+  variables :graphite_host => graphite_host
 end
 
 cron "ganglia_graphite" do
