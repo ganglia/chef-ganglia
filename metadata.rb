7088a758670cc5b794655208fa3de9d2950d41b0
Joseph Anthony Pasquale Holsten <joseph@josephholsten.com>
2012-11-20 13:25:16 +0000

    FC007: Ensure recipe dependencies are reflected in cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "ganglia"
 maintainer       "Heavy Water Software Inc."
 maintainer_email "darrin@heavywater.ca"
 license          "Apache 2.0"
@@ -11,4 +12,3 @@ end
 
 recommends "graphite"
 suggests "iptables"
-
