16fed2ea50573f397545482c12256390c6c9f1b7
Ben Hartshorne <ben@parse.com>
2014-03-03 09:29:33 -0800

    changing the maintainer from Heavy Water to the Ganglia Team, bumping the version of the cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "Heavy Water Software Inc."
-maintainer_email "darrin@heavywater.ca"
+maintainer       "Ganglia Team"
+maintainer_email "ganglia-developers@lists.sourceforge.net"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.1"
+version          "0.2.0"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
