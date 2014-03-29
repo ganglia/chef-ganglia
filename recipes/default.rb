787396518193861016fbce37590c13ab7ecc50ad
Ben Hartshorne <ben@parse.com>
2014-03-29 15:28:27 -0700

    collectors should be an array if no collectors are found

--- recipes/default.rb
+++ recipes/default.rb
@@ -60,7 +60,7 @@ when true
     gmond_collectors = search(:node, "role:#{node['ganglia']['server_role']} AND chef_environment:#{node.chef_environment}").map {|node| node.ipaddress}
   end rescue NoMethodError
   if gmond_collectors.empty? 
-     gmond_collectors = "127.0.0.1"
+     gmond_collectors = ["127.0.0.1"]
   end
 
   template "/etc/ganglia/gmond.conf" do
