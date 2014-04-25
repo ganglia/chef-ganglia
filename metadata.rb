329d2d5a63292805035f49cc5a4746e624327178
José de Paula Eufrásio Júnior <jose.junior@gmail.com>
2014-04-25 19:36:31 -0400

    Fixes gmetad.conf template typo

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer_email "ganglia-developers@lists.sourceforge.net"
 license          "Apache 2.0"
 description      "Installs/Configures ganglia"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.5"
+version          "0.3.6"
 
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
